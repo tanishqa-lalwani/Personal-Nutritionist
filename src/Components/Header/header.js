a986f6735302114e793368dc2fe2996a82cd6096
Kushagra Pathak <45987083+defalt18@users.noreply.github.com>
2021-03-18 13:55:39 +0530

    Update Header
    Updating header to follow through as other components get added

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,30 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './header.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import logo from './pineapple-icon.png'
 import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button';
 import Drawer from '../Mobile Drawer/Drawer'
 import SignUpModal from '../../Components/Modals/SignUpModal'
+import { useLocation } from 'react-router-dom'
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 function Header() {
 
+    const location = useLocation();
+
     const handlescroll = () => {
-        if(window.scrollY>50)
-        {
+        if (window.scrollY > 50) {
             document.getElementsByClassName('header')[0].style.background = 'rgba(255,255,255,1)';
             document.getElementsByClassName('header')[0].style.boxShadow = '0 0 5px 0 rgba(0,0,0,0.5)';
         }
-        else
-        {
-            document.getElementsByClassName('header')[0].style.boxShadow = '0 0 0 0 rgba(0,0,0,0.5)';
-            document.getElementsByClassName('header')[0].style.background='transparent';
+        else {
+            if (location.pathname.includes('dashboard') === 0) {
+                document.getElementsByClassName('header')[0].style.boxShadow = '0 0 0 0 rgba(0,0,0,0.5)';
+                document.getElementsByClassName('header')[0].style.background = 'transparent';
+            }
         }
     }
 
-    window.addEventListener('scroll',handlescroll);
+    window.addEventListener('scroll', handlescroll);
 
-    console.log(logo);
+
+    useEffect(() => {
+        if (location.pathname.includes('dashboard') > 0) {
+            document.getElementsByClassName('header')[0].style.background = 'rgba(255,255,255,1)';
+            document.getElementsByClassName('header')[0].style.borderBottom = '1px solid #321e59';
+            document.getElementsByClassName('header')[0].style.boxShadow = '0 0 0 0';
+
+        }
+        else {
+            document.getElementsByClassName('header')[0].style.boxShadow = '0 0 0 0 rgba(0,0,0,0.5)';
+            document.getElementsByClassName('header')[0].style.background = 'transparent';
+            document.getElementsByClassName('header')[0].style.borderBottom = '0px solid #321e59';
+        }
+    }, [location])
 
     return (
         <div className='header'>
@@ -32,34 +50,49 @@ function Header() {
                 <Drawer />
             </div>
             <Link to='/'>
-                <Avatar src = {logo}/>
+                <Avatar src={logo} />
             </Link>
-            <Link className='links' to='/food'  style={{textDecoration:'none'}}>
-            <div className='two'>
-                Food
+            <Link className='links' to='/food' style={{ textDecoration: 'none' }}>
+                <div className='two'>
+                    Food
             </div>
             </Link>
-            <Link className='links' to='/recipes' style={{textDecoration:'none'}}>
+            <Link className='links' to='/recipes' style={{ textDecoration: 'none' }}>
                 <div className='two'>
                     Recipes
                 </div>
             </Link>
-            <Link className='links' to='/blogs' style={{textDecoration:'none'}}>
-            <div className='two'>
-                Blogs
+            <Link className='links' to='/blogs' style={{ textDecoration: 'none' }}>
+                <div className='two'>
+                    Blogs
             </div>
             </Link>
-            <div className='two'> 
+            <div className='two'>
                 About Us
             </div>
-            <div className='four'>
-            <Button variant="outlined" style={{borderRadius:'10px',textTransform:'capitalize',padding:0,fontFamily:'Poppins, sans-serif',border:'0px solid #321E59',color:'#321E59'}}>
-                <SignUpModal/>
-            </Button>
-            <Button variant="outlined" style={{borderRadius:'10px',textTransform:'capitalize',fontFamily:'Poppins, sans-serif',border:'1px solid #321E59',color:'#321E59'}}>
-                Login
-            </Button>
-            </div>
+            {
+                location.pathname === "/userclass/dashboard" ?
+                    (
+                        <div className="four" style={{ alignItems: "center" }}>
+                            <NotificationsIcon />
+                            <div className="header__user">
+                                <Avatar style={{ width: "30px", height: '30px' }} />
+                                <p>Username</p>
+                                <ArrowDropDownIcon />
+                            </div>
+                        </div>
+                    ) :
+                    (
+                        <div className='four'>
+                            <Button variant="outlined" style={{ borderRadius: '10px', textTransform: 'capitalize', padding: 0, fontFamily: 'Poppins, sans-serif', border: '0px solid #321E59', color: '#321E59' }}>
+                                <SignUpModal />
+                            </Button>
+                            <Button variant="outlined" style={{ borderRadius: '10px', textTransform: 'capitalize', fontFamily: 'Poppins, sans-serif', border: '1px solid #321E59', color: '#321E59' }}>
+                                Login
+                    </Button>
+                        </div>
+                    )
+            }
         </div>
     )
 }
