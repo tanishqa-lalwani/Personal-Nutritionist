fc132539994dd763ce5b6d7a4aedce6b7047b66f
Kushagra Pathak <45987083+defalt18@users.noreply.github.com>
2021-03-21 18:22:11 +0530

    Update CircularComponent.js

--- src/Components/Circular_Progress/CircularComponent.js
+++ src/Components/Circular_Progress/CircularComponent.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import './CircularComponent.css'
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Newcomp from './SubComp'
 
-function CircularComponent({ color }) {
+
+function CircularComponent({ color, size, thick, value ,text}) {
     return (
         <div className="bada_circ">
             <div className="circ1">
-                <CircularProgress className="circle1" variant="determinate" thickness={10} size={100} value={75} style={{ color: `${color}` }} />
+                <Newcomp value={value} thick={thick} size={size} color={color} text={text}/>
             </div>
             <div className="circ2">
-                <CircularProgress variant="determinate" thickness={10} size={100} value={100} />
+                <CircularProgress variant="determinate" thickness={thick} size={size} value={100} /> 
             </div>
         </div>
     )
