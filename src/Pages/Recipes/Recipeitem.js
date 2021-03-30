8f084100f18cfd01a370ada32dedb6062e35a557
Aakash Dabhi <aaku.9724980577@gmail.com>
2021-03-30 13:24:15 +0530

    Recipes Instructions Added

--- src/Pages/Recipes/Recipeitem.js
+++ src/Pages/Recipes/Recipeitem.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import './Recipeitem.css'
-import img from './image 1.png'
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -8,52 +7,140 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Button from '@material-ui/core/Button';
+import CloseIcon from '@material-ui/icons/Close';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     cards: {
         border:'2px solid #9CBEFF',
     },
-  });
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+      
+    },
+  }));
 
-function Recipeitem({foodimg, foodname, foodcal}) {
-    const classes = useStyles();
+function Recipeitem({foodname,foodimg,foodcal,foodfat,foodcarbs,foodprotein,foodrecipe}) {
+     const classes = useStyles();
+     const [open, setOpen] = React.useState(false);
+     const handleOpen = () => {setOpen(true);};
+     const handleClose = () => {setOpen(false);};
     return (
-    <>
-        <Card className={classes.cards}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    alt={foodname}
-                    height="230"
-                    image={foodimg}
-                    title={foodname}
-                />
-                <CardContent>
-                <Typography gutterBottom variant="body2" component="h2">
-                {foodname}
-                </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions>
-                <div className="stats">
-                    <div className="statitem">
-                            <div className="statvalue">
+          <Card className={classes.cards} >
+              <CardActionArea>
+                  <CardMedia
+                      component="img"
+                      alt={foodname}
+                      height="230"
+                      image={foodimg}
+                      title={foodname}
+                  />
+                  <CardContent>
+                  <Typography gutterBottom variant="body2" component="h2">
+                    {foodname}
+                  </Typography>
+                  </CardContent>
+              </CardActionArea>
+              <CardActions>
+                  <div className="stats">
+                      <div className="statitem">
+                              <div className="statvalue">
                                 {Math.round(foodcal)}
-                            </div>
-                            <p>CALORIES</p>
-                        </div>
-                        <div className="statitem">
-                            <div className="statvalue">
-                                55
-                            </div>
-                            <p>MINUTES</p>
-                        </div>
-                    </div>
-            </CardActions>
-        </Card>
-    </>
-    )
+                              </div>
+                              <p>CALORIES</p>
+                          </div> 
 
+                <div className="statitem">
+                <Button variant="contained" color="primary" id="View_btn" onClick={handleOpen} >
+                  View Recipe
+                </Button>
+                </div>
+                
+              <div className="Recipe_Modal">
+                <Modal
+                  aria-labelledby="transition-modal-title"
+                  aria-describedby="transition-modal-description"
+                  className={classes.modal}
+                  open={open}
+                  onClose={handleClose}
+                  closeAfterTransition
+                  BackdropComponent={Backdrop}
+                  BackdropProps={{
+                    timeout: 500,
+                  }}
+                  style={{
+                    backdropFilter: 'saturate(180%) blur(10px)',
+                }}
+                >
+                  <Fade in={open}>
+                  <div className="Recipe_info" > 
+                    <div className="close_btn">
+                        <CloseIcon onClick={handleClose} style={{ background: 'rgba(0,150,255)', padding: '8px', borderRadius: '50%', color: 'white' }} />
+                    </div> 
+
+                      <div className="Nutrients_Info">
+                        <h2>Nutrients: </h2>
+                        <div className="Nutrients_box">
+
+                            <div className="Nutrients_stats">
+                              <p>Calories</p>  
+                              <div className="Nutrients_values">
+                                {Math.round(foodcal)} kcal
+                              </div>
+                            </div>
+
+                            <div className="Nutrients_stats">
+                              <p>Protein</p>  
+                              <div className="Nutrients_values">
+                                {foodprotein} gm
+                              </div>
+                            </div>
+
+                            <div className="Nutrients_stats">
+                              <p>Carbs</p>  
+                              <div className="Nutrients_values">
+                                {foodcarbs} gm
+                              </div>
+                            </div>
+
+                            <div className="Nutrients_stats">
+                              <p>Fat</p>  
+                              <div className="Nutrients_values">
+                                {foodfat} gm
+                              </div>
+                            </div>
+
+                        </div>
+                      </div>
+
+                      <div className="Recipe_Instructions">  
+                        <h2>Instructions: </h2>
+                        <div className="Instruction">
+                        <ol>
+                        {foodrecipe.map((steps) =>  
+                            <li key={steps.number}>{steps.step}</li>)}
+                        </ol>
+                        </div>
+                      </div>
+                    </div>
+                  </Fade>
+                </Modal>
+               </div>
+              </div>
+            </CardActions>
+          </Card>
+       )
 }
 
-export default Recipeitem
+export default Recipeitem;
--- src/Pages/Recipes/Recipes.js
+++ src/Pages/Recipes/Recipes.js
@@ -4,30 +4,30 @@ import FilterListIcon from '@material-ui/icons/FilterList';
 import SearchIcon from '@material-ui/icons/Search';
 import Recipeitem from './Recipeitem';
 import Button from '@material-ui/core/Button';
+import Footer from '../../Components/Footer/footer'
 
 function Recipes() {
 
     const [recipes, setRecipes] = useState([]);
     const [search,setSearch] = useState("");
     const [query,setQuery] = useState("");
-
+    const API_KEY = '0e06064897434c9ea1e85457f3fb7b26'
+    
     useEffect(() => {
         getRecipes();
     }, [query]);
 
-
     const getRecipes = async () => {
 
-        const { RecipeSearchClient } = require('edamam-api');
- 
-        const client = new RecipeSearchClient({
-        appId:'b9bc11a0',
-        appKey: 'f486e302156a516812a67e312759ac3f'
-        });
-    
-        const results = await client.search({query});
-        setRecipes(results.hits);
-        console.log(results.hits);
+       
+            try {
+            const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${API_KEY}&instructionsRequired=true&addRecipeInformation=true&addRecipeNutrition=true`)
+            const json = await response.json();  
+            setRecipes(json.results)
+            }
+            catch (error) {
+                console.log(error);
+              }  
     }
 
     const searchItem = item => {
@@ -38,8 +38,8 @@ function Recipes() {
         item.preventDefault();
         setQuery(search);
     }
-
     return (
+        <>
         <div style={{width:'100vw'}}>
             <div className="recipe__head">
                 <h1>Find Recipes</h1>
@@ -55,24 +55,26 @@ function Recipes() {
                 <form className="search" onSubmit={getItem}>
                 <div className="searchbox">
                     <input type='text' placeholder="Search for recipes" value={search} onChange={searchItem}/>
-                    <Button id="pc" variant="filled" style={{fontFamily:'Poppins, sans-serif',textTransform:'capitalize',color:'black',}} onClick={getItem}>
+                    <Button onClick={getItem}>
                     <SearchIcon /> 
                     </Button>
                 </div>
                 </form>
 
+                <div className="Filter_btn">
                 <Button id="pc" variant="filled" style={{background:'#699DFF',fontFamily:'Poppins, sans-serif',textTransform:'capitalize',color:'white',}}>
                     <FilterListIcon/> Apply Filter
                 </Button>
                 <Button id="mobile" variant="filled" style={{background:'#699DFF',fontFamily:'Poppins, sans-serif',textTransform:'capitalize',color:'white',}}>
                     <FilterListIcon/>
                 </Button>
+                </div>
             </div>
             <div id='searchmob'>
                 <form className="search" onSubmit={getItem}>
                     <div className="searchbox">
                         <input type='text' placeholder="Search for recipes" value={search} onChange={searchItem}/>
-                        <Button id="mobile" variant="filled" style={{fontFamily:'Poppins, sans-serif',textTransform:'capitalize',color:'black',}} onClick={getItem}>
+                        <Button onClick={getItem}>
                         <SearchIcon /> 
                         </Button>
                     </div>
@@ -84,20 +86,22 @@ function Recipes() {
                     <FilterListIcon/>
                 </Button>
             </div>
-            <div className="recipes">
-
-                {recipes!== [] && recipes.map(recipe => (
+           <div className="recipes">
+                {recipes !== [] && recipes.map(recipe => (  
                     <Recipeitem 
-                    key={recipe.recipe.label}
-                    title={recipe.recipe.label} 
-                    calories={recipe.recipe.calories} 
-                    image = {recipe.recipe.image}/>
-                    
+                    key={recipe.id}
+                    foodname={recipe.title}  
+                    foodimg = {recipe.image}
+                    foodcal={recipe.nutrition.nutrients[0].amount}
+                    foodfat={recipe.nutrition.nutrients[1].amount}
+                    foodcarbs={recipe.nutrition.nutrients[3].amount}
+                    foodprotein={recipe.nutrition.nutrients[8].amount}
+                    foodrecipe={recipe.analyzedInstructions[0].steps}/>
                 ))}
-                
             </div>
+            <Footer/>
         </div>
+        </>
     )
-}
-
+  }    
 export default Recipes;
