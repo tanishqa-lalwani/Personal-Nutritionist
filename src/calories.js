33443311b6d27809806e5ca5862fdb0d20f21174
aksharrrrr <aksharrughani@gmail.com>
2021-05-03 20:10:21 +0530

    Algorithm Updated

--- src/calories.js
+++ src/calories.js
@@ -1,10 +1,32 @@
 //gender male: 1       female: 0
-var userHeight = 1.422;
-var userWeight = 41;
+
+
+// low activity = 1.3
+//moderate activity = 1.5
+// high activity = 1.7
+
+//userTarget = 0 to loose weight
+//userTarget = 1 to to maintain weight
+//userTarget = 2 to to gain weight
+
+var userHeight = 1.77;
+var userWeight = 80;
 var userAge = 20;
-var userGender = 0;
-var userActivity = 1.7;
-var userGoal = 50;
+var userGender = 1;
+var userActivity = 1.5;
+var userGoal = 80;
+var userCurrentWeight = 75;
+var userTarget = 2;
+
+
+
+
+//weeks calculator 
+function HowManyWeeks(goal, current_weight) {
+    const diff = Math.abs(goal - current_weight);
+    const noOfWeeks = diff/0.45 ;
+    return noOfWeeks;
+}
 
 //BMI calculator
 function BmiCalculator(weight , height) {
@@ -28,82 +50,51 @@ function idealWeight (height){
 }
 
 
-//if user want to go with ideal weight
-function CaloriesWithoutGoal(age, height, gender, activity) {
-    var median_ideal_weight = idealWeight(height);
-    
+
+
+
+//function to calculate calories rn
+function CaloriesForCurrentWeight(age, gender, activity, current_weight){
     //male
     if(gender == 1) {
         switch(true){
             case (age>=10 && age<=17):
-                return ((17.7 * median_ideal_weight) +657)* activity;
+                return ((17.7 * current_weight) +657)* activity;
                 break;
             case (age>=18 && age<=29):
-                return ((15.1 * median_ideal_weight) +692)* activity;
+                return ((15.1 * current_weight) +692)* activity;
                 break;
             case (age>=30 && age<=59):
-                return ((11.5 * median_ideal_weight) +873)* activity;
+                return ((11.5 * current_weight) +873)* activity;
                 break;
             case (age>=60):
-                return ((11.7 * median_ideal_weight) +585)* activity;
+                return ((11.7 * current_weight) +585)* activity;
                 break;
         }
     //female
     }else {
         switch(true){
             case (age>=10 && age<=17):
-                return ((13.4 * median_ideal_weight) +692)* activity;
+                return ((13.4 * current_weight) +692)* activity;
                 break;
             case (age>=18 && age<=29):
-                return ((14.8 * median_ideal_weight) +487)* activity;
+                return ((14.8 * current_weight) +487)* activity;
                 break;
             case (age>=30 && age<=59):
-                return ((8.3 * median_ideal_weight) +846)* activity;
+                return ((8.3 * current_weight) +846)* activity;
                 break;
             case (age>=60):
-                return ((9.0 * median_ideal_weight) +656)* activity;
+                return ((9.0 * current_weight) +656)* activity;
                 break;
         }
     }
     
 }
 
-//function with goal
-function CaloriesWithGoal(age, gender, activity, goal){
-    //male
-    if(gender == 1) {
-        switch(true){
-            case (age>=10 && age<=17):
-                return ((17.7 * goal) +657)* activity;
-                break;
-            case (age>=18 && age<=29):
-                return ((15.1 * goal) +692)* activity;
-                break;
-            case (age>=30 && age<=59):
-                return ((11.5 * goal) +873)* activity;
-                break;
-            case (age>=60):
-                return ((11.7 * goal) +585)* activity;
-                break;
-        }
-    //female
-    }else {
-        switch(true){
-            case (age>=10 && age<=17):
-                return ((13.4 * goal) +692)* activity;
-                break;
-            case (age>=18 && age<=29):
-                return ((14.8 * goal) +487)* activity;
-                break;
-            case (age>=30 && age<=59):
-                return ((8.3 * goal) +846)* activity;
-                break;
-            case (age>=60):
-                return ((9.0 * goal) +656)* activity;
-                break;
-        }
-    }
-    
+function DailyCaloriesIntake(currentCalories, target) {
+    if (target == 0) return currentCalories - 500;
+    else if( target == 1)  return currentCalories;
+    else return currentCalories + 500;
 }
 
 
@@ -113,9 +104,15 @@ console.log(idealWeight(userHeight));
 //BMI calculator 
 console.log(BmiCalculator(userWeight, userHeight));
 
-//with goal
-console.log(CaloriesWithGoal(userAge, userGender, userActivity, userGoal));
 
-//without goal
-console.log(CaloriesWithoutGoal(userAge, userHeight, userGender, userActivity));
+//calculate no of weeks
+console.log(HowManyWeeks(userGoal, userCurrentWeight))
+
+//to calculate calories with curretn weight
+var calories = CaloriesForCurrentWeight(userAge, userGender, userActivity, userCurrentWeight);
+console.log(calories);
+
+//calories to lose, gain weight or to maintain weight
+console.log(DailyCaloriesIntake(calories, userTarget));
+
 
